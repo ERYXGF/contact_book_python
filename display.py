15fda37ec408bbdcae02e8267ecd7a7c01878c18
Eryx G.F <eryxgrammaticas@gmail.com>
2026-03-09 18:32:02 +0100

    feat: display_menu func

--- display.py
+++ display.py
@@ -2,3 +2,24 @@
 If you ever want to change how something looks visually you only touch this file.
 This is the view layer of the application, completely separate from the business logic in contacts.py and the data handling in file_handler.py. 
 It should only be responsible for taking data and printing it to the screen in a nice format."""
+
+def display_menu():
+    print("Welcome to the CLI Contact Book!")
+    print("1. View all contacts")
+    print("2. Add a contact")
+    print("3. Search for a contact")
+    print("4. Edit a contact")
+    print("5. Delete a contact")
+    print("6. View Favourites")
+    print("7. View by category")
+    print("8. View statistics")
+    print("9. Export to a .txt file")
+    print("0. Quit/Exit from the Contact Book")
+    while True:
+        try:
+            decision = int(input("Enter your choice (0-9): "))
+            break
+        except ValueError:
+            print("Invalid choice. Please enter a number (0-9).")
+    return decision
+
