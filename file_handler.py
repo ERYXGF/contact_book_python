09bf049123cf8b53d507cde0a5695350858187d4
Eryx G.F <eryxgrammaticas@gmail.com>
2026-03-15 19:30:27 +0100

    feat: save func

--- file_handler.py
+++ file_handler.py
@@ -2,3 +2,11 @@
 All the error handling for corrupt files lives here too. 
 No other file touches the JSON directly — they all go through this file.
 """
+
+#Import JSON Library
+import json
+
+#Function that saves contacts to a json file:
+def save(contacts, path = "contacts.json"):
+    with open(path, "w", encoding  = "utf-8") as f:
+        json.dump(contacts, f, ensure_ascii = False, indent = 4)
