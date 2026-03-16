3fe47e8b9f6954c68234996881c2d6baf783951d
Eryx G.F <eryxgrammaticas@gmail.com>
2026-03-16 15:36:08 +0100

    chore: condition that executes main func

--- main.py
+++ main.py
@@ -3,12 +3,18 @@ It as the manager who delegates everything but does no actual work itself.
 Handles input.
 It should be short — ideally under 60 lines."""
 
-contact_input = {
-    "name" : input("What is the contact's full name (both first and last one) ? "),
-    "phone" : input("What is the contact's phone number ? "),
-    "email" : input("What is the contact's email ? "),
-    "city" : input("From which city is the contact from ?"),
-    "category" : input("Please choose a category: "),
-    "favourite" : input("Do you wish to add this contact as a favourite (Y or N) ? "),
-    "birthday" : input("What is the person's birthday ?"),
-}
+#Main function that ties everything together:
+def main():
+    contact_input = {
+        "name" : input("What is the contact's full name (both first and last one) ? "),
+        "phone" : input("What is the contact's phone number ? "),
+        "email" : input("What is the contact's email ? "),
+        "city" : input("From which city is the contact from ?"),
+        "category" : input("Please choose a category: "),
+        "favourite" : input("Do you wish to add this contact as a favourite (Y or N) ? "),
+        "birthday" : input("What is the person's birthday ?"),
+    }
+
+#Initiates the function that ties everything together (main):
+if __name__ == "main.py":
+    main()
